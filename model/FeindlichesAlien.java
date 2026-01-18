8bd322ece4b2ed5e4e9e6eb6f488e57178ee1103
Hanna <hanna.luebken@Student.HTW-Berlin.de>
2026-01-18 21:51:39 +0100

    Kindklassen Alien

---
+++ model/FeindlichesAlien.java
@@ -0,0 +1,12 @@
+/**
+ * Eine konkrete Kindklasse von Alien.
+ * @author Hanna Lübken
+ */
+public class FeindlichesAlien extends Alien {
+    private static final long serialVersionUID = 1L;
+
+    public FeindlichesAlien() {
+        super("Grimmiger Marsbewohner", 15, false, "Du hättest besser mal zuhause bleiben sollen!");
+    }
+    
+}
---
+++ model/FreundlichesAlien.java
@@ -0,0 +1,11 @@
+/**
+ * Eine konkrete Kindklasse von Alien.
+ * @author Hanna Lübken
+ */
+public class FreundlichesAlien extends Alien {
+    private static final long serialVersionUID = 1L; 
+
+    public FreundlichesAlien() {
+        super("Riesenschnecke", 20, true, "Heyy, konntest du schon alle Übungsleiter finden?");
+    }
+}
