45929599b6ec56a0f5a079ce575abb309adb7d37
Hanna <hanna.luebken@Student.HTW-Berlin.de>
2026-01-18 21:55:06 +0100

    überarbeitung Kindklasse

--- model/FeindlichesAlien.java
+++ model/FeindlichesAlien.java
@@ -3,7 +3,6 @@
  * @author Hanna Lübken
  */
 public class FeindlichesAlien extends Alien {
-    private static final long serialVersionUID = 1L;
 
     public FeindlichesAlien() {
         super("Grimmiger Marsbewohner", 15, false, "Du hättest besser mal zuhause bleiben sollen!");
--- model/FreundlichesAlien.java
+++ model/FreundlichesAlien.java
@@ -2,8 +2,7 @@
  * Eine konkrete Kindklasse von Alien.
  * @author Hanna Lübken
  */
-public class FreundlichesAlien extends Alien {
-    private static final long serialVersionUID = 1L; 
+public class FreundlichesAlien extends Alien { 
 
     public FreundlichesAlien() {
         super("Riesenschnecke", 20, true, "Heyy, konntest du schon alle Übungsleiter finden?");
